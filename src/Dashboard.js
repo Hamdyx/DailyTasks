7764022dd4d7739addd1ef6364b8fa513d92383a
Cryptojoint <a.hamdy.nig@gmail.com>
2021-08-17 14:19:09 +0200

    Dashboard refactor
    seperated some UI parts into functional components with initial style,
    > Schedule with at least 14 days displayed
    > some appointments or tasks to do in the specified date

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Col, Container, Row, Button, Form } from 'react-bootstrap';
+import { IoNotificationsSharp, IoMailSharp } from 'react-icons/io5';
 
 import './Dashboard.css';
 import avatarPic from './avatar.png';
@@ -24,15 +25,25 @@ export const Dashboard = () => {
 					</Container>
 				</Col>
 				<Col>
-					<img src={avatarPic} alt="logo" className="avatar-pic" />
-					<p>Cryptojoint</p>
-					<p>Developer</p>
-					<h6>Profile UI</h6>
-					<div>
-						<p>Project time tracker</p>
-						<p>You can start tracking</p>
-						<Button>x</Button>
-					</div>
+					<DashboardProfile />
+					<ProjectTracker />
+					<DashboardSchedule />
+				</Col>
+			</Row>
+		</Container>
+	);
+};
+
+const DashboardSchedule = () => {
+	return (
+		<Container className="dashboard-schedule">
+			<Row>
+				<Col>
+					<p className="dashboard-fulldate">August 10, 2021</p>
+					<p className="dashboard-day">Today</p>
+				</Col>
+				<Col>
+					<Button className="dashboard-addTask">+ add task</Button>
 				</Col>
 			</Row>
 		</Container>
@@ -45,6 +56,46 @@ const TaskComponent = () => {
 			<h6>Task</h6>
 			<p>some description about this task</p>
 			<p>Progress</p>
+		</Container>
+	);
+};
+
+const ProjectTracker = () => {
+	return (
+		<Container className="dashboard-procjet-tracker">
+			<Row>
+				<Col>
+					<p>Project time tracker</p>
+					<p>You can start tracking</p>
+				</Col>
+				<Col>
+					<Button>X</Button>
+				</Col>
+			</Row>
+		</Container>
+	);
+};
+
+const DashboardProfile = () => {
+	return (
+		<Container className="dashboard-profile">
+			<Row>
+				<Col sm={{ span: 4 }}>
+					<img src={avatarPic} alt="logo" className="avatar-pic" />
+				</Col>
+				<Col className="profile-info">
+					<p>Cryptojoint</p>
+					<p>Developer</p>
+				</Col>
+				<Col>
+					<Button className="profile-btn">
+						<IoNotificationsSharp />
+					</Button>
+					<Button className="profile-btn">
+						<IoMailSharp />
+					</Button>
+				</Col>
+			</Row>
 		</Container>
 	);
 };
